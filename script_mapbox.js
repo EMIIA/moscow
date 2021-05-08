d8fbf36b89df696a1fc4e167b73b7fbb15dbccfb
ЭМИИА <vstarostin@emiia.ru>
2021-05-08 21:14:26 +0300

    Update script_mapbox.js

--- script_mapbox.js
+++ script_mapbox.js
@@ -1,10 +1,10 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW1paWFhaSIsImEiOiJja21icnU4bHkyNGRwMnFrbjVvNXdtdGJ0In0.TcfxV21Ov7zOgvWvgPxzlA';
     var map = new mapboxgl.Map({
         style: 'mapbox://styles/emiiaai/ckofqwhvv1ujy17p27p0a6cr8',
-        center: [37.543, 55.715],
-        zoom: 7.59,
-        pitch: 0,
-        bearing: -17.6,
+        center: [37.6334, 55.7627],
+        zoom: 8.94,
+        pitch: 40,
+        bearing: 0,
         container: 'map',
         antialias: true
     });
