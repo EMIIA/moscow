beb0570ebff29ae9f0e553d3fe725abeded57144
ЭМИИА <vstarostin@emiia.ru>
2021-05-10 19:27:36 +0300

    Update and rename mapbox-gl.js to map-gl.js

--- map-gl.js
+++ map-gl.js
@@ -1,4 +1,3 @@
-/* Mapbox GL JS is Copyright © 2020 Mapbox and subject to the Mapbox Terms of Service ((https://www.mapbox.com/legal/tos/). */
 (function (global, factory) {
 typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 typeof define === 'function' && define.amd ? define(factory) :
